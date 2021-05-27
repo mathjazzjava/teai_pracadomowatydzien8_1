0e0022b7fbf1162a22da58819eccdc7e92f807b5
mathjazz <mathjazzjava@gmail.com>
2021-05-27 12:14:22 +0200

    Add score4

--- src/main/java/com/mathjazz/hibernate1/Start.java
+++ src/main/java/com/mathjazz/hibernate1/Start.java
@@ -21,11 +21,13 @@ public class Start {
         Score score1 = new Score("12:30", 25.5F);
         Score  score2 = new Score("13:30", 24.0F);
         Score  score3 = new Score("14:30", 23.0F);
+        Score  score4 = new Score("15:30", 23.0F);
 
 //        scoreRepo.save(score1);
 //        scoreRepo.save(score2);
 //        scoreRepo.save(score3);
 //        scoreRepo.deleteById(1L);
+        scoreRepo.save(score4);
         scoreRepo.findAll().forEach(System.out::println);
 
     }
